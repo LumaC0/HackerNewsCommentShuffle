3c4439fd737084101007d2517a545d88f0b5cfe2
LumaC0 <finsp01@gmail.com>
2022-11-06 18:06:08 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,175 @@
+() => {
+  /**
+    Check and set a global guard variable.
+    If this content script is injected into the same page again,
+    it will do nothing next time.
+   */
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
+};
+
+const contentScript = {
+  js: "content.js",
+  regex: /^.*news.ycombinator.com\/item\?id=.*/g,
+};
+
+/* 
+  Loading content scripts dynamically (loading and unloading) is not supported out
+  of the box. I invision the user clicking the browser action to shuffle the
+  comment tree. Subsequent clicks emulate on/off functionality.
+
+  Sync extension storage is used to direct dynamic content script
+  loading/unloading
+
+  schema like so:
+    {tab.id: {
+        loaded: true,
+        shuffled: false
+    }}
+
+  "loaded" indicates whether the content script is loaded into a given tab
+  "shuffled" alternates boolean values on each click.
+*/
+
+let storageGet = async (tabId) => {
+  let store;
+
+  try {
+    store = await browser.storage.sync.get(tabId);
+  } catch (err) {
+    new Error(err);
+  }
+
+  return Object.keys(store).length == 0 ? undefined : store[tabId];
+};
+
+
+let storageSet = async (tabId, /* database value */ val) => {
+  try {
+    await browser.storage.sync.set({ [tabId]: val });
+  } catch (err) {
+    new Error(err);
+  }
+};
+
+
+let storageRemove = async (tabId) => {
+  let sync = browser.storage.sync;
+
+  try {
+    tabId === undefined 
+      ? await sync.clear() 
+      : await sync.remove(tabId);
+  } catch (err) {
+    new Error(err);
+  }
+};
+
+
+let loadContentScript = async (tab) => {
+  try {
+    await browser.scripting.executeScript({
+      target: {
+        tabId: tab.id,
+        allFrames: true
+      },
+      injectImmediately: true,
+      files: [contentScript.js],
+    });
+  } catch (err) {
+    new Error(err)
+  }
+
+  await storageSet(tab.id.toString(), 
+    {
+      loaded: true,
+      shuffle: false,
+    }
+  );
+};
+
+
+let sendMessage = async (tab) => {
+  let shuffle, updateBody;
+
+  while (true) {
+
+    try {
+      shuffle = await storageGet(tab.id.toString());
+    } catch (err) {
+      new Error(err)
+    }
+
+    updateBody = !shuffle.shuffle
+      ? {loaded: true, shuffle: true}
+      : {loaded: true, shuffle: false};
+
+    try {
+      await browser.tabs.sendMessage(tab.id, {
+        command: updateBody.shuffle,
+      });
+    } catch (err) {
+      // sendMessage fails if content script detaches
+      // removing the requisite storage key, will allow for reinjection
+      // awaiting these function calls works 
+      await storageRemove(tab.id.toString());
+      await loadContentScript(tab);
+      continue;
+    }
+
+    break;
+
+  }
+  await storageSet(tab.id.toString(), updateBody);
+};
+
+
+let clickActions = async (tab) => {
+  if (tab.url.match(contentScript.regex)) {
+    const loaded = await storageGet(tab.id.toString());
+
+    if (loaded == undefined) {
+      await loadContentScript(tab);
+      await sendMessage(tab);
+
+    } else {
+      await sendMessage(tab);
+    }
+
+  } else {
+    // remove entry from db if tab url does not match. A user navigating back to
+    // the HN landing detaches the content script.
+    await storageRemove(tab.id.toString());
+  }
+};
+
+
+/* without a popup, clicking the browser action sends a click event to the 
+ * background script 
+*/
+browser.action.onClicked.addListener(async (tab) => {
+  await clickActions(tab);
+});
+
+
+/* This block listens for messages from content scripts
+ * and clears the calling tab's data if no comments exist.
+ * This does not unregister the content script, so reinitialization 
+ * raises for redefining variables 
+*/
+// browser.runtime.onConnect.addListener((port) => {
+//   const portFromCS = port;
+
+//   portFromCS.onMessage.addListener(async (message) => {
+//     if (!message.status) {
+
+//       browser.tabs.query({active: true})
+//         // .then(tabs => browser.tabs.get(tabs[0].id))
+//         .then(async tabs => {
+//           await storageRemove(tabs[0].id.toString());
+//         });
+//     }
+//   });
+// });
---
+++ content.js
@@ -0,0 +1,92 @@
+/*
+  Currently able to replace the comment tree with an arbitrary html element.
+
+  TODO: Shuffle comment groups and cast to an HTMLCollection for html
+  replacement
+
+  TODO: follow the DOM backward to replace ad-hoc element with original html
+*/
+
+let toHTMLTable = (/* array of comment groups */ commentGroups) => {
+  const tableBody = document.createElement("tbody");
+
+  for (i of commentGroups.flat()) tableBody.appendChild(i);
+
+  return tableBody;
+};
+
+let shuffleTree = (/* array of comments */ comments) => {
+  let treeGroups = groupComments(comments);
+
+  for (let i = treeGroups.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [treeGroups[i], treeGroups[j]] = [treeGroups[j], treeGroups[i]];
+  }
+
+  return toHTMLTable(treeGroups);
+};
+
+let commentTreePath = (/* true for DOM parent node */ parent) => {
+  const commentTree = document.getElementsByClassName("comment-tree")[0];
+
+  return parent === true
+    ? commentTree /* table */
+    : commentTree.childNodes[1]; /* tbody */
+};
+
+let groupComments = (comments) => {
+  let commentGroups = [];
+  let commentGroupIndex;
+
+  for (i of comments) {
+    let indent = i.getElementsByClassName("ind")[0].getAttribute("indent");
+
+    if (indent === "0") {
+      commentGroupIndex = commentGroups.push([i]);
+    } else {
+      commentGroups[commentGroupIndex - 1].push(i);
+    }
+  }
+
+  return commentGroups;
+};
+
+const defaultCommentArray = (() => {
+  commentNodes = commentTreePath((parent = false));
+
+  // HTMLCollection of <tr> comments
+  return commentNodes ? Array.from(commentNodes.children) : null;
+})();
+
+let shuffleCommentTree = () => {
+  const newTree = shuffleTree(defaultCommentArray); /* tbody element */
+  const table = commentTreePath((parent = true));
+
+  table.replaceChild(newTree, commentTreePath((parent = false)));
+};
+
+let unshuffleCommentTree = async () => {
+  const defaultTree = toHTMLTable(defaultCommentArray);
+  const table = commentTreePath((container = true));
+
+  table.replaceChild(defaultTree, commentTreePath((container = false)));
+};
+
+// const port = browser.runtime.connect({ name: "port-from-cs" });
+browser.runtime.onMessage.addListener((message) => {
+  // if tbody is not in node list
+  if (defaultCommentArray !== null) {
+    if (message.command) {
+      console.log("shuffle comments");
+      shuffleCommentTree();
+
+    } else {
+      console.log("unshuffle comments");
+      unshuffleCommentTree();
+    }
+  } else {
+    // do not communicate to background script
+    // port.postMessage({ status: false });
+    console.log("no comments to shuffle");
+  }
+});
